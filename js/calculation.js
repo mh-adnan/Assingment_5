48dd364e1d9f0606afa75dd6a80083d713a7f588
adnan <mehedihassanadnan@gmail.com>
2024-09-23 02:15:34 +0600

    getting amount via js done

---
+++ js/calculation.js
@@ -0,0 +1,4 @@
+document.getElementById('new_khali_donate_now').addEventListener('click', function() {
+    const amount = getBalanceFromInput('new_khali_amount');
+    console.log("new khali amount : ",amount);
+});
---
+++ js/comonFunction.js
@@ -0,0 +1,4 @@
+function getBalanceFromInput(id) {
+    const inputData = parseFloat(document.getElementById(id).value);
+    return inputData;
+}
